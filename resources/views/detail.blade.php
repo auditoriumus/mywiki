2040ab86cc3349d1b28417b67c129b722be928fd
root <root@248421.simplecloud.ru>
2020-08-17 14:05:59 +0300

    Проект развернут на сервере

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -7,13 +7,15 @@
             <h5 class="card-title">@php echo $result['title'] @endphp</h5>
             <p class="card-text">@php echo $result['note'] @endphp</p>
         </div>
-        <a href="{{ route('data.edit', $result['id']) }}" class="btn btn-secondary">Изменить</a>
-        <div class="row">
-        <form class="col-12" action="{{ route('data.destroy', $result['id']) }}" method="post">
-            @csrf
-            <input type="hidden" name="_method" value="DELETE">
-            <button type="submit" class="col-12 btn btn-danger">Удалить</button>
-        </form>
-        </div>
+        @if(Auth::user()->email == 'um_2005@mail.ru')
+            <a href="{{ route('data.edit', $result['id']) }}" class="btn btn-secondary">Изменить</a>
+            <div class="row">
+            <form class="col-12" action="{{ route('data.destroy', $result['id']) }}" method="post">
+                @csrf
+                <input type="hidden" name="_method" value="DELETE">
+                <button type="submit" class="col-12 btn btn-danger">Удалить</button>
+            </form>
+            </div>
+        @endif
     </div>
 @endsection
